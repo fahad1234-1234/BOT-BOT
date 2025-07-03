1c5998aaa3e870a11e4211b25e6e19b8e03f1b95
Fahad <md.fahad.226262@gmail.com>
2025-07-03 08:02:06 +0600

    Update and rename cmdstore.js to cmdstore5.js

--- scripts/cmds/cmdstore5.js
+++ scripts/cmds/cmdstore5.js
@@ -6,7 +6,7 @@ const ITEMS_PER_PAGE = 10;
 
 module.exports.config = {
   name: "cmdstore",
-  aliases: ["cs", "cmds"],
+  aliases: ["cs5", "cmds5"],
   author: "Dipto",
   role: 2,
   version: "6.9",
